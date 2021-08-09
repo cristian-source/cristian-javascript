2d1bc7f1ce564216c6c10b760377526458d2f7cd
Cristian <78826502+cristian-source@users.noreply.github.com>
2021-08-09 20:00:58 -0300

    Add files via upload

---
+++ Proyecto/js/javascript.js
@@ -0,0 +1,27 @@
+function cambiarColorFondo() {
+    document.body.style.backgroundColor= '#ede2e0';
+
+}
+
+function cambiarColor() {
+    var parrafo = document.getElementsByTagName("p");
+    parrafo[0].style.color= '#ee4d2f'
+    parrafo[1].style.color= '#ee4d2f'
+    parrafo[2].style.color= '#ee4d2f'
+    parrafo[3].style.color= '#ee4d2f'
+    parrafo[4].style.color= '#ee4d2f'
+    parrafo[5].style.color= '#ee4d2f'
+}
+
+function tamañoFuente() {
+    document.getElementById('destacado').style.fontSize= '24px'
+
+}
+function cambiarTipografía() {
+    document.getElementById('fuente').style.fontFamily= 'calibri'
+} 
+
+function mostrarImagen() {
+    var imagen= document.getElementById('imagen')
+    imagen.style.display= 'block';
+} 
---
+++ Proyecto/js/javascript1.js
@@ -0,0 +1,2 @@
+var nombre= prompt ('Bienvenid@')
+document.write ('<p>Bienvenid@ ' + nombre)
